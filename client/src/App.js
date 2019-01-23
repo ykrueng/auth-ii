e65bd735a09de572a4b4ec47b17213f85a5e3f28
ykrueng <yasirah.krueng@gmail.com>
2019-01-23 13:05:21 -0600

    basic user list

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Segment, Header } from "semantic-ui-react";
+import { Segment, Header, Card } from "semantic-ui-react";
 import { Route } from "react-router-dom";
 import axios from "axios";
 
@@ -10,7 +10,29 @@ class App extends React.Component {
     isLoggedIn: false,
     token: null,
     error: null,
+    users: null,
   };
+
+  getUsers = async (token) => {
+    try {
+      const users = await axios({
+        url: `http://localhost:8000/api/protected/users`,
+        headers: { authorization: token }
+      });
+
+      this.setState({
+        users: users.data
+      })
+
+    } catch (err) {
+      console.log(err);
+      this.setState({
+        isLoggedIn: false,
+        token: null,
+        error: err,
+      })
+    }
+  }
 
   authenticate = async (account, signup) => {
     const baseURL = `http://localhost:8000/api/`;
@@ -20,6 +42,8 @@ class App extends React.Component {
         `${baseURL}${signup ? 'register' : 'login'}`,
         account
       );
+
+      this.getUsers(auth.data.token);
 
       this.setState({
         isLoggedIn: true,
@@ -37,7 +61,7 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoggedIn } = this.state;
+    const { isLoggedIn, users } = this.state;
     return (
       <Segment
         style={{
@@ -48,6 +72,12 @@ class App extends React.Component {
         <Header as="h1" textAlign="center">
           Welcome to Auth-II
         </Header>
+        {
+          users &&
+          users.map(user => (
+            <Card key={user.id}>{user.username}</Card>
+          ))
+        }
         <Route
           exact
           path="/signup"
